66150065bb98ec6b75fdda85ce9301161d4516b8
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-22 00:24:18 -0300

    refactor(MenuLink): update to NavLink

--- src/components/MenuLink/MenuLink.jsx
+++ src/components/MenuLink/MenuLink.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, useLocation } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './MenuLink.module.scss';
 
 function MenuLink({ children, to }) {
-  const location = useLocation();
-
   return (
-    <Link
-      className={`
+    <NavLink
+      className={({ isActive }) => `
         ${styles.link}
-        ${location.pathname === to ? styles.active : ''}
+        ${isActive && styles.active}
       `}
       to={to}
     >
       {children}
-    </Link>
+    </NavLink>
   );
 }
 
