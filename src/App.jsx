7c2509c5ac19176a874d124f13f78edd638a0312
Beto Sardinha <sardinhabeto55@gmail.com>
2023-06-21 23:41:15 -0300

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from 'pages/Home';
+import About from 'pages/About';
 
 function App() {
   return (
-    <div className="App">
-      Curriculum
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
