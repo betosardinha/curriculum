942675778698ad01960e263beef7933096bb44fc
Beto Sardinha <sardinhabeto55@gmail.com>
2023-07-20 19:40:23 -0300

    feat: add Post page

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
+
+import PostTemplate from 'components/PostTemplate';
+
+import posts from 'data/posts.json';
+
+import styles from './Post.module.scss';
+
+function Post() {
+  const params = useParams();
+  const currentPost = posts.find((post) => post.id === Number(params.id));
+
+  return (
+    <PostTemplate
+      cover={`/assets/images/${currentPost.id}/cover.png`}
+      title={currentPost.title}
+    >
+      <div className={styles.post}>
+        <ReactMarkdown>
+          {currentPost.content}
+        </ReactMarkdown>
+      </div>
+    </PostTemplate>
+  );
+}
+
+export default Post;
