9c54d79faa5857b490e16d297d5c0b0c7e55b3b5
Beto Sardinha <sardinhabeto55@gmail.com>
2023-07-30 20:19:43 -0300

    feat: add recommended posts

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,6 +5,7 @@ import ReactMarkdown from 'react-markdown';
 import NotFound from 'pages/NotFound';
 import PostTemplate from 'components/PostTemplate';
 import DefaultPage from 'components/DefaultPage';
+import PostCard from 'components/PostCard';
 
 import posts from 'data/posts.json';
 
@@ -13,6 +14,10 @@ import styles from './Post.module.scss';
 function Post() {
   const params = useParams();
   const currentPost = posts.find((post) => post.id === Number(params.id));
+  const recommendedPosts = posts
+    .filter((post) => post.id !== Number(params.id))
+    .sort((a, b) => b.id - a.id)
+    .slice(0, 4);
 
   if (!currentPost) {
     return <NotFound />;
@@ -29,6 +34,19 @@ function Post() {
             {currentPost.content}
           </ReactMarkdown>
         </div>
+
+        <h2 className={styles.recommendedpoststitle}>
+          Outros posts que você pode gostar
+        </h2>
+
+        <ul className={styles.recommendedposts}>
+          {recommendedPosts.map((post) => (
+            <li key={post.id}>
+              <PostCard post={post} />
+            </li>
+          ))}
+        </ul>
+
       </PostTemplate>
     </DefaultPage>
   );
